9ecff62ff7f44c1a2dd862f34600c80269922259
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-11-15 18:44:29 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import { useState } from 'react';
-import { Header } from './components/Header';
+import { Header } from './components/header/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
+import { Sidebar } from './components/sidebar/Sidebar';
 import './global.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <Header />
-      <Post 
-        author="Gabriel Morais"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat veniam ex a culpa impedit! Vitae, delectus, totam quia officia sunt assumenda at, ducimus repellat temporibus iusto libero odit cupiditate."
-        
-      />
-    
 
-    <Post 
-        author="Luana Morais"
-        content="Lorem ipsum POST."
-        
-      />
+      <div className={styles.wrapper}>
 
+        <aside>
+          <Sidebar />
+        </aside>
+        
+        <main>
+          <Post 
+            author="Gabriel Morais"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat veniam ex a culpa impedit! Vitae, delectus, totam quia officia sunt assumenda at, ducimus repellat temporibus iusto libero odit cupiditate."
+            
+          />
+        
+
+          <Post 
+              author="Luana Morais"
+              content="Lorem ipsum POST."
+            />
+        </main>
+        
+
+      </div>
+      
     </div>
   )
 }
