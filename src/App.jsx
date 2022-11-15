a4f2023eb1b00a2bd34abddd635cc01c3d423943
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-11-15 15:25:41 -0300

    Construindo estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import { Post } from './Post'
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './styles.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-    <Post 
-      author="Gabriel Morais"
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat veniam ex a culpa impedit! Vitae, delectus, totam quia officia sunt assumenda at, ducimus repellat temporibus iusto libero odit cupiditate."
-      
-    />
-   
+      <Header />
+      <Post 
+        author="Gabriel Morais"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat veniam ex a culpa impedit! Vitae, delectus, totam quia officia sunt assumenda at, ducimus repellat temporibus iusto libero odit cupiditate."
+        
+      />
+    
 
-   <Post 
-      author="Luana Morais"
-      content="Lorem ipsum POST."
-      
-    />
+    <Post 
+        author="Luana Morais"
+        content="Lorem ipsum POST."
+        
+      />
 
     </div>
   )
