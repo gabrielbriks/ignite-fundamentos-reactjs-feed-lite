9cc4f947c0ec1cfc176b5b43d39101401ca2c24d
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-11-15 19:15:14 -0300

    Fincaliza construção da sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,14 +9,20 @@ export function Sidebar() {
         className={styles.cover} 
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
+      
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://lh3.googleusercontent.com/a/ALm5wu2Yy41vwaqg5bb3gni4I4ixN7qufWJ5Ur3nKMCc-g=s288-p-rw-no" 
+        />
         <strong>Gabriel Morais</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
