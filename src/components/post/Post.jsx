9508fc33a4fa36f97f9506ec86b079199408d6b1
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-11-26 12:04:26 -0300

    Removendo a utilização do indice na prop key
    Após compreender o porque não devemos utilizar o indice na propriedade
    Key dentro do react, realizando ajuste ,

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -64,12 +64,12 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.content}>
         {
-          content.map((line, index) => {
+          content.map(line => {
             if(line.type === 'paragraph') {
-              return <p key={index}>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             }
             else if(line.type === 'link'){
-              return <p key={index}><a href=''>{line.content}</a></p>              
+              return <p key={line.content}><a href=''>{line.content}</a></p>              
             }
           })
         }
@@ -94,8 +94,8 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {
-          comments.map((comment, index) => {
-           return <Comment content={comment} key={index}/>
+          comments.map(comment => {
+           return <Comment content={comment} key={comment}/>
           })
         }
       </div>
